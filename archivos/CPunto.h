79d56877fec2a14937a09e0fdc4a51feaa8de872
davento <50033456+davento@users.noreply.github.com>
2019-11-28 11:13:52 -0500

    Update CPunto.h

--- archivos/CPunto.h
+++ archivos/CPunto.h
@@ -13,17 +13,9 @@ public:
     CPunto (const CPunto &otroPunto);
     entero getX();
     entero getY();
-    entero getG();
-    entero getF();
-    entero getH();
-    entero getTempG();
     texto getName();
     simbolo getMark();
     ~CPunto ();
-    void setG(entero _g);
-    void setF(entero _f);
-    void setH(entero _h);
-    void setTempG(entero _tg);
     void setX(entero _x);
     void setY(entero _y);
     void setName(texto _name);
