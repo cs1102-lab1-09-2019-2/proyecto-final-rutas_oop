b8e2864ba964f9356b2e005f52439d71355a455c
jarod429 <54276838+jarod429@users.noreply.github.com>
2019-11-27 23:56:15 -0500

    Update proyectAssets.h

--- archivos/proyectAssets.h
+++ archivos/proyectAssets.h
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <time.h>
+#include <string.h>
 #include <stdlib.h> 
 #include <iomanip>
 #include <string>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <climits>
 
 using namespace std;
 
 typedef int entero;
 typedef char simbolo;
 typedef string texto;
+typedef bool booleano;
