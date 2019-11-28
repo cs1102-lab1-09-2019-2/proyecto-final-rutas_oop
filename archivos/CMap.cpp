4c8f688a418a60b0dbf12636c52d5054c010a68e
jarod429 <54276838+jarod429@users.noreply.github.com>
2019-11-27 23:54:58 -0500

    Update CMap.cpp

--- archivos/CMap.cpp
+++ archivos/CMap.cpp
@@ -1,58 +1,69 @@
 #include "CMap.h"
 
+void CMap::generarMapa() {
+    M=new simbolo*[alto];
+    for (int i=0;i<alto;i++)
+      M[i]=new simbolo[ancho];
+
+  entero a;
+    for(int i=0;i<alto;i++){
+      for(int j=0;j<ancho;j++){ 
+        if(i==0){
+          a = rand()%2;
+          if (a==0) {M[i][j]='0';}
+          else {M[i][j]='1';}
+        } else
+            if (i<alto-1){
+                if(M[i-1][j+1]=='1')
+                    M[i][j]='0';
+                else
+                    { a = rand()%2;
+                      if (a==0) {M[i][j]='0';}
+                      else {M[i][j]='1';}
+                    }
+            } else {
+               if (M[i-1][j]==1)
+                   M[i][j]='0';
+               else
+                   {  a = rand()%2;
+                      if (a==0) {M[i][j]='0';}
+                      else {M[i][j]='1';}
+              }
+          }
+      }
+  }
+}
 CMap::CMap() {
     ancho=defAncho;
     alto=defAlto;
     cantPuntos=defPunto;
-    generarMapa(ancho, alto);
 }
 
-CMap::CMap(entero _ancho, entero _alto) {
-    cantPuntos=0;
-    if (x>defAncho) {
+CMap::CMap (entero _ancho, entero _alto) {
+      cantPuntos=0;
+    if (_ancho>defAncho)
         ancho = _ancho;
-    } if (y>defAlto)
+    if (alto>defAlto)
         alto = _alto;
-    } else {
+    else {
         ancho = defAncho;
         alto = defAlto;
     }
-    generarMapa(ancho, alto);
 }
 
-void CMap::generarMapa(entero ancho, entero alto) {
-
-    M=new simbolo*[alto];
-    for (int i=0;i<alto;i++)
-    M[i]=new simbolo[ancho];
-
-    for(int i=0;i<alto;i++){
-      for(int j=0;j<ancho;j++){
-        if(i==0)
-            M[i][j]=(simbolo)rand()%2;
-        else
-            if (i<alto-1){
-                if(M[i-1][j+1]==1)
-                    M[i][j]=(simbolo)0;
-                else
-                    M[i][j] =(simbolo)rand()%2;  
-            } else {
-               if (M[i-1][j]==1)
-                   M[i][j]=(simbolo)0;
-               else
-                   M[i][j] =(simbolo)rand()%2;
-            }
-    }
+void CMap::updateMap() {
+    for (int i = 0; i < cantPuntos; i++)
+        M[puntos[i]->getX()][puntos[i]->getY()]=puntos[i]->getMark();
 }
 
-void CMap::addPunto(CPunto* _punto) {
-     CPunto** temp = new CPunto*[cantPuntos + 1];
-     for (int i = 0; i < cantPuntos; ++i)
-         temp[i] = puntos[i];
-     temp[cantPuntos]= punto;
-     cantPuntos++;
+void CMap::updateList() {
+  puntos = new CPunto*[cantPuntos];
 }
 
+void CMap::addPunto(CPunto* _punto, entero i) {
+     M[_punto->getX()][_punto->getY()] = _punto->getMark();
+     puntos[i] = _punto;
+}
 CPunto* CMap::buscarPunto(texto _nombre) {
     for (int i = 0; i < cantPuntos; ++i) {
         if (_nombre == puntos[i]->getName())
@@ -61,57 +72,14 @@ CPunto* CMap::buscarPunto(texto _nombre) {
     return NULL;
 }
 
-CPunto* CMap::removerPunto(texto _nombre) {
-    auto punto = buscarPunto(_nombre);
-
-    if (punto == NULL)
-       return NULL;
-
-    CPunto** temp = new CPunto*[cantPuntos-1];
-    int j=0;
-    for (int = 0; i < cantPuntos; i++) {
-        if (puntos[i] ! = punto) {
-            temp[j]=puntos[i];
-            j++
-        } else {
-            M[puntos[i]->getX()][puntos[i]->getY()]='0';
-        }
+bool CMap::verificarNombre(texto nombre) {
+  for (int i = 0; i< cantPuntos; i++) {
+    if (nombre == puntos[i]->getName()) {
+      return true;
+      }
     }
-    puntos = temp;
-    cantPuntos--;
-    return punto;
-}
-
-void CMap::modificarPunto(*CPunto _punto) {
-            entero newX, newY;
-            simbolo newMark;
-            texto newName;
-            do {
-                cout<<"\nIngrese nuevo valor para la coordenada x: ";
-                cin>>newX;
-            } while (newX > mapa->getAncho() || newX<0);
-            do {
-            cout<<"\nIngrese nuevo valor para la coordenada y: ";
-            cin>>newY;
-            } while (newY > mapa->getAlto() || newY<0);
-            do {
-            cout<<"\nIngrese nuevo marcador para el punto: ";
-            cin>>newMark;
-            } while (newMark=='1' || newMark=='0');
-            cout<<"\Ingrese nuevo nombre para el punto: ";
-            cin>>newName;
-            M[_punto->getX()][_punto->getY()]='0';
-            _punto->setX(newX);
-            _punto->setY(newY);
-            _punto->setMark(newMark);
-            _punto->setName(newName);
-            mapa->updateMap();
-}
-
-void CMap::updateMap() {
-    for (int i =0; i < cantPuntos; i++)
-        M[puntos[i]->getX()][puntos[i]->getY()]=puntos[i]->getMark();
-}
+    return false;
+  }
 
 void CMap::imprimirMap() {
     for (int i=0; i<alto; i++){
@@ -125,7 +93,7 @@ void CMap::imprimirMap() {
 void CMap::imprimirPuntos() {
     for (int i = 0; i < cantPuntos; i++) {
         cout<<"\n"<<puntos[i]->getName()<<" | "<<puntos[i]->getMark()
-        <<"en ("<<puntos[i]->getX()<<", "<<puntos[i]->getY()<<")";
+        <<" está en ("<<puntos[i]->getX()<<", "<<puntos[i]->getY()<<")";
     }
 }
 
@@ -141,11 +109,72 @@ entero CMap::getCantPuntos() {
     return cantPuntos;
 }
 
+simbolo** CMap::getMap() {
+    return M;
+}
+
 CMap::~CMap() {
     for (int i= 0; i<alto; i++) {
         delete [] M[i];
     }
-
     delete [] M;
     M = NULL;
+}
+
+booleano CMap::isValid(booleano visited[20][20], entero row, entero col)
+{
+ return (row >= 0) && (row < alto) && (col >= 0) && (col < ancho)
+  && M[row][col] && !visited[row][col];
+}
+
+void CMap::BFS(entero i, entero j, entero x, entero y)
+{
+ int row[] = { -1, 0, 0, 1 };
+ int col[] = { 0, -1, 1, 0 };
+ char temp=M[x][y];
+ booleano visited[20][20];
+
+
+
+memset(visited, false, sizeof (visited));
+
+ queue<CNode> q;
+
+ visited[i][j] = true;
+ q.push({i, j, 0});
+
+ entero min_dist = INT_MAX;
+
+ while (!q.empty()) 
+ {
+  CNode node = q.front();
+  q.pop();
+  entero i = node.x, j = node.y, dist = node.dist;
+  if (i == x && j == y) 
+  {
+   min_dist = dist;
+   break;
+  }
+
+  for (int k = 0; k < 4; k++) 
+  {
+   if (isValid(visited, i + row[k], j + col[k]) && M[i+row[k]][j+col[k]] != '1')
+   {int n,m;
+    visited[i + row[k]][j + col[k]] = true;
+
+    M[i+row[k]][j+col[k]] = '-';
+    q.push({ i + row[k], j + col[k], dist + 1 });
+    n=i+row[k];
+    m=j+col[k];
+   }
+  }
+   M[x][y]=temp;
+ }
+
+ if (min_dist != INT_MAX)
+  { imprimirMap();
+    cout << "La distancia más corta tiene longitud "
+     << min_dist<<"\n";}
+ else
+  cout << "No es posible calcular la ruta"<<"\n";
 }
